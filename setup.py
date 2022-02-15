c6a0d1e9c504d198b738f4bcb219ff88845979d0
chriscauley <chris@lablackey.com>
2022-02-15 15:33:20 -0500

    removing requirements as it was causing issues with deploy

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
 
 import re
-from pathlib import Path
-import pkg_resources
 from setuptools import setup, find_namespace_packages
 
 _version_re = re.compile(r"__version__\s+=\s+(.*)")
 
-
 with open("feed_checker/__init__.py", "r") as f:
     version = _version_re.search(f.read()).group(1).strip("'\"")
 
-with Path("requirements.txt").open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement in pkg_resources.parse_requirements(requirements_txt)
-    ]
-
-
-long_description = Path("README.md").read_text()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name="calitp_feed_checker",
     version=version,
     packages=find_namespace_packages(),
-    install_requires=install_requires,
+    install_requires=[
+        "beautifulsoup4",
+        "python-dotenv",
+        "PyYAML",
+        "requests",
+        "typer",
+    ],
     description="Tool for checking if transit urls are on aggregator websites",
     long_description=long_description,
     long_description_content_type="text/markdown",
